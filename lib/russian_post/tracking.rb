d7a40113cf7827dcb1eee9f4e503ad652514cec9
Artem Shitov <inbox@artemshitov.ru>
2013-04-27 15:18:20 +0400

    Mechanize instead of Excon
    This helps abstracting most of page processing

--- lib/russian_post/tracking.rb
+++ lib/russian_post/tracking.rb
@@ -1,17 +1,15 @@
-require 'excon'
+require 'mechanize'
 require 'nokogiri'
 
 require 'time'
-require 'cgi'
 
 module RussianPost
 
   class Tracking
 
-    attr_reader :barcode, :current_page, :current_html
+    attr_reader :barcode, :agent
 
     TRACKING_PAGE = 'http://www.russianpost.ru/rp/servise/ru/home/postuslug/trackingpo'
-    ACTION_URL = 'http://www.russianpost.ru/resp_engine.aspx?Path=rp/servise/ru/home/postuslug/trackingpo'
     COLUMNS = [
       :type,
       :date,
@@ -23,47 +21,33 @@ module RussianPost
       :delivery_cash,
       :destination_zip_code,
       :destination_location]
-    GENERIC_HEADERS = {
-      'User-Agent'   => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 Safari/537.22',
-      'Referer'      => 'http://www.russianpost.ru/resp_engine.aspx?Path=rp/servise/ru/home/postuslug/trackingpo',
-      'Origin'       => 'http://www.russianpost.ru',
-      'Content-Type' => 'application/x-www-form-urlencoded'}
-
+    
     def initialize(tracking_code)
       @barcode = tracking_code.strip.upcase
+      @agent   = Mechanize.new
     end
 
     def track
-      fetch_initial_page!
-      request_tracking_data!
-      
-      if current_html.css("table.pagetext")
-        parse_tracking_table(current_html)
-      else
-        raise "No tracks table in response"
-      end
+      initial_page = fetch_initial_page
+      initial_page.form.set_fields(
+        'BarCode'            => barcode,
+        'InputedCaptchaCode' => solve_captcha(initial_page.images.last.src),
+        'searchsign'         => '1')
+
+      tracking_page = initial_page.form.submit
+      parse_tracking_table(tracking_page.search(".pagetext tbody tr"))
     end
 
     private
 
-    def solve_captcha
-      captcha = RussianPost::Captcha.for_url(parse_captcha_url)
+    def solve_captcha(captcha_url)
+      captcha = RussianPost::Captcha.for_url(captcha_url)
       raise "Unable to recognize captcha" unless captcha.valid?
       captcha.text
     end
 
-    def parse_request_form
-      current_html.css("input").reduce({}) do |acc, result|
-        acc.merge Hash[result.attr("name"), result.attr("value")]
-      end
-    end
-
-    def parse_captcha_url
-      current_html.css("#captchaImage").attr("src") or raise "Unable to extract captcha image url"
-    end
-
     def parse_tracking_table(body)
-      body.css('table.pagetext tbody tr').map { |e| parse_row(e) }
+      body.map { |e| parse_row(e) }
     end
 
     def parse_row(row)
@@ -77,42 +61,15 @@ module RussianPost
       row.css('td').map { |td| td.text unless ['-', ''].include?(td.text) }
     end
 
-    def request_tracking_data!
-      request_data = encode_params(prepare_params)
-
-      response = Excon.post(ACTION_URL, 
-        :headers => {
-          'Cookie' => prepare_cookies,
-          'Content-Length' => request_data.size }.merge(GENERIC_HEADERS),
-        :body => request_data)
-
-      set_current_page!(response)
-    end
-
-    def prepare_params
-      parse_request_form.merge(
-        'BarCode'            => barcode,
-        'InputedCaptchaCode' => solve_captcha,
-        'searchsign'         => '1' # strictly required
-      )
-    end
-
-    def prepare_cookies
-      current_page.headers['Set-Cookie'].scan(/[\w\.]+=[\w\-]+(?=;)/).join("; ")
-    end
-
-    def encode_params(params)
-      params.map do |name, value|
-        "#{CGI.escape(name.to_s)}=#{CGI.escape(value.to_s)}"
-      end.join('&')
-    end
-
-    def fetch_initial_page!
-      response = Excon.get(TRACKING_PAGE)
-      if response.body =~ /<input id=\"key\" name=\"key\" value=\"([0-9]+)\"\/>/ # tough security huh
-        response = Excon.post(TRACKING_PAGE, body: "key=#{$1}")
+    def fetch_initial_page
+      page = agent.get(TRACKING_PAGE)
+      if page.form.has_field?("key")
+        page = initial_page.form.submit
+      elsif page.body.include?("window.location.replace(window.location.toString())")
+        page = fetch_initial_page
       end
 
+<<<<<<< HEAD
       if response.body.include?("window.location.replace(window.location.toString())") # hehe
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -132,8 +89,9 @@ module RussianPost
     def set_current_page!(response)
       @current_page = response
       @current_html = Nokogiri::HTML(response.body)
+=======
+      page
+>>>>>>> Mechanize instead of Excon
     end
-
   end
-
 end
